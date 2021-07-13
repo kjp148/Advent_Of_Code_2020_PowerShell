f5a9fa77c4a30e5a35a30615c5d3677ee924a1da
Keanan Paul <keananpaul@yahoo.com>
2021-07-13 00:57:39 -0700

    Finished Day 3 Part 2

--- day3.ps1
+++ day3.ps1
@@ -1,15 +1,30 @@
-$horiz = 0
-$TreeCount = 0
+[string[]]$Map = (Get-Content .\Input\day3.txt)
+$TreesMultiplied = 1
 
-foreach ($Line in (Get-Content .\Input\day3.txt))
+# Part 2 modifiers (index 1 is part 1)
+[int[]]$VertModifier = (1, 1, 1, 1, 2)
+[int[]]$HorizModifier = (1, 3, 5, 7, 1)
+
+for ([int]$ModifierI = 0; $ModifierI -lt $VertModifier.Length; $ModifierI++)
 {
-    if ($Line[$horiz] -eq '#')
+    $Vert = 0
+    $Horiz = 0
+    $TreeCount = 0
+    for ([int]$Vert = 0; $Vert -lt $Map.Length; $Vert += $VertModifier[$ModifierI])
     {
-        $TreeCount++
+        if ($Map[$Vert][$Horiz] -eq '#')
+        {
+            $TreeCount++
+        }
+        $Horiz += $HorizModifier[$ModifierI]
+        $Horiz %= $Line.Length # Wrap
     }
 
-    $horiz += 3 # Down one over three (Down is covered by foreach)
-    $horiz %= $Line.Length # Wrap
+    if ($ModifierI -eq 1) # part 1
+    {
+        Write-Host "Day 3 Part 1: $($TreeCount)"
+    }
+    $TreesMultiplied *= $TreeCount
 }
 
-Write-Host "Day 3 Part 1: $($TreeCount)"
+Write-Host "Day 3 Part 2: $($TreesMultiplied)"
